5154649dcd64c4f87167fe8c7c73316618e96cf2
TapanKumarYadav <95100517+TapanKumarYadav@users.noreply.github.com>
2023-08-05 10:33:13 +0530

    First Commit

---
+++ src/dummyData.js
@@ -0,0 +1,142 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture: "/assets/person/1.jpeg",
+        username: "Anjali Singh",
+    },
+    {
+        id:2,
+        profilePicture: "/assets/person/2.jpeg",
+        username: "Ravi Mishra",
+    },
+    {
+        id:3,
+        profilePicture: "/assets/person/3.jpeg",
+        username: "Tanya Rathore",
+    },
+    {
+        id:4,
+        profilePicture: "/assets/person/4.jpeg",
+        username: "Aarti Gupta",
+    },
+    {
+        id:5,
+        profilePicture: "/assets/person/5.jpeg",
+        username: "Smriti Yadav",
+    },
+    {
+        id:6,
+        profilePicture: "/assets/person/6.jpeg",
+        username: "Kriti Jaiswal",
+    },
+    {
+        id:7,
+        profilePicture: "/assets/person/7.jpeg",
+        username: "Saurabh Pandey",
+    },
+    {
+        id:8,
+        profilePicture: "/assets/person/8.jpeg",
+        username: "Ragini Patel",
+    },
+    {
+        id:9,
+        profilePicture: "/assets/person/9.jpeg",
+        username: "Ritika Srivastava",
+    },
+    {
+        id:10,
+        profilePicture: "/assets/person/10.jpeg",
+        username: "Rashmi Agnihotri",
+    },
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Love For All, Hatred For None.",
+        photo: "/assets/post/1.jpeg",
+        date: "5 mins ago",
+        usedId: 1,
+        like: 32,
+        comment: 9
+    },
+    {
+        id: 2,
+        desc: "Today's BreakFast",
+        photo: "/assets/post/2.jpeg",
+        date: "10 mins ago",
+        usedId: 2,
+        like: 10,
+        comment: 2
+    },
+    {
+        id: 3,
+        desc: "Next Vacation will be",
+        photo: "/assets/post/3.jpeg",
+        date: "15 mins ago",
+        usedId: 3,
+        like: 45,
+        comment: 8
+    },
+    {
+        id: 4,
+        desc: "I love fruits", 
+        photo: "/assets/post/4.jpeg",
+        date: "20 mins ago",
+        usedId: 4,
+        like: 3,
+        comment: 0
+    },
+    {
+        id: 5,
+        desc: "Cooking is Love",
+        photo: "/assets/post/5.jpeg",
+        date: "25 mins ago",
+        usedId: 5,
+        like: 67,
+        comment: 10
+    },
+    {
+        id: 6,
+        desc: "My new pet",
+        photo: "/assets/post/6.jpeg",
+        date: "30 mins ago",
+        usedId: 6,
+        like: 54,
+        comment: 7
+    },
+    {
+        id: 7,
+        photo: "/assets/post/7.jpeg",
+        date: "35 mins ago",
+        usedId: 7,
+        like: 420,
+        comment: 45
+    },
+    {
+        id: 8,
+        photo: "/assets/post/8.jpeg",
+        date: "40 mins ago",
+        usedId: 8,
+        like: 173,
+        comment: 28
+    },
+    {
+        id: 9,
+        desc: "Our pre-wedding shoot",
+        photo: "/assets/post/9.jpeg",
+        date: "45 mins ago",
+        usedId: 9,
+        like: 132,
+        comment: 17
+    },
+    {
+        id: 10,
+        photo: "/assets/post/10.jpeg",
+        date: "50 mins ago",
+        usedId: 10,
+        like: 84,
+        comment: 15
+    }
+];
